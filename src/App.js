915da70837fe05ff2637ff415b0d406a91a87d01
Ejiscoexpress <ejiscoexpress@gmail.com>
2023-10-03 16:50:52 +0100

    useEffect commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-import React from 'react';
+import {useEffect}from 'react';
 
 // dcbabb41
 const API_URL 'http://www.omdbapi.com?apikey=dcbabb41';
 
 const App= () => {
+
+    useEffect(() => {
+
+    }, []);
     return (
         <h1>Movie App</h1>
     );
