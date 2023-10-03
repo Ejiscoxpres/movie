e43c7016ebe59813fde7254dc3dc2545bc4c4e27
Ejiscoexpress <ejiscoexpress@gmail.com>
2023-10-03 17:00:25 +0100

    title commit

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ const API_URL 'http://www.omdbapi.com?apikey=dcbabb41';
 
 const App= () => {
     const searchMovies = async (title) => {
-        const response = await fetch(``);
+        const response = await fetch(`${API_URL}&s=${title}`);
 
     }
 
