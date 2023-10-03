c1dd374ec63d1237b8164de550545a5d7d7aea12
Ejiscoexpress <ejiscoexpress@gmail.com>
2023-10-03 17:07:36 +0100

    console commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import {useEffect}from 'react';
 
 // dcbabb41
-const API_URL 'http://www.omdbapi.com?apikey=dcbabb41';
+const API_URL= 'http://www.omdbapi.com?apikey=dcbabb41';
 
 const App= () => {
     const searchMovies = async (title) => {
         const response = await fetch(`${API_URL}&s=${title}`);
          const data = await response.json();
+          console.log(data);
 
     }
 
