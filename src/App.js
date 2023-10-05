ce3852662e37205efa9fff92db304e3e544322a2
Ejiscoexpress <ejiscoexpress@gmail.com>
2023-10-05 13:18:17 +0100

    useState commit

--- src/App.js
+++ src/App.js
@@ -15,11 +15,11 @@ const API_URL= 'http://www.omdbapi.com?apikey=dcbabb41';
  }
 
 const App= () => {
-    const [movies, setMovies] = useState();
+    const [movies, setMovies] = useState([]);
     const searchMovies = async (title) => {
         const response = await fetch(`${API_URL}&s=${title}`);
          const data = await response.json();
-          console.log(data.search);
+          setMovies(data.search);
 
     }
 
